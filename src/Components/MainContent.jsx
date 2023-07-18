279474c874ff5137617a09d67504e395ecd80c88
avinash-kr-pandey <emails4avinash@gmail.com>
2023-07-18 09:56:34 +0530

    commit 1807

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -39,9 +39,9 @@ const MainContent = () => {
           <div className="card text-white bg-success mb-3" style={{ maxWidth: "18rem" }}>
           <h4>A3</h4>
             <div className='labelData'>
-              <label for="label1" className="label">Total Students</label>
+              <label for="label1" className="label">Total Students:</label>
               <label for="label2" className="label">500</label> <br />
-              <label for="label1" className="label">Present</label>
+              <label for="label1" className="label">Presents:</label>
               <label for="label2" className="label">450</label>
             </div>
           </div>
@@ -55,9 +55,9 @@ const MainContent = () => {
           <div className="card text-white bg-danger mb-3" style={{ maxWidth: "18rem" }}>
           <h4>B1</h4>
             <div className='labelData'>
-              <label for="label1" className="label">Total Students</label>
+              <label for="label1" className="label">Total Students: </label>
               <label for="label2" className="label">300</label> <br />
-              <label for="label1" className="label">Present</label>
+              <label for="label1" className="label">Present:</label>
               <label for="label2" className="label">255</label>
             </div>
           </div>
@@ -66,10 +66,10 @@ const MainContent = () => {
           <div className="card text-white bg-warning mb-3" style={{ maxWidth: "18rem" }}>
           <h4>B2</h4>
             <div className='labelData'>
-              <label for="label1" className="label">Total</label>
-              <label for="label2" className="label">Label 2:</label> <br />
-              <label for="label1" className="label">Label 1:</label>
-              <label for="label2" className="label">Label 2:</label>
+              <label for="label1" className="label">Total Students: </label>
+              <label for="label2" className="label">100</label> <br />
+              <label for="label1" className="label">Present:</label>
+              <label for="label2" className="label">60:</label>
             </div>
           </div>
         </div>
